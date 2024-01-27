3a895afcf62fc96ec4cce44e148822db54dbb920
John Gilliland <John.Gilliland@Starz.com>
2024-01-27 01:26:49 -0700

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import getRandomJoke from "./jokes";
+import './styles/main.scss';
+import laughing from './assets/laughing.svg';
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', getRandomJoke);
